07ea9fbe2727908802dc8f7870f15c137f200f8d
GriffinPeirce <griff.peirce@gmail.com>
2019-09-17 20:01:26 -0700

    Somewhat working valve states and raw flowmeter logging

--- ColdFlowGUI/test.py
+++ ColdFlowGUI/test.py
@@ -18,41 +18,34 @@ from matplotlib.backends.backend_qt4agg import (
 class MyDialog(QtGui.QDialog):
     def __init__(self, parent=None):
         QtGui.QWidget.__init__(self, parent)
-        self.mainValveState = False
-        self.ventValveState = False
         self.logData = False
-        self.mainValveAddress = 2016 #CIO1
-        self.ventValveAddress = 2017 #CIO0
+        self.ventValveAddress = 2016 #CIO0
+        self.mainValveAddress = 2017 #CIO1
         self.flowMeterAddress = 6 # AIN3
         self.tankPressureAddress = 2 # AIN1
         self.outletPressureAddress = 0 # AIN0
         self.clock20Hz = 61522
-        self.actuatorsAddresses = [self.mainValveAddress, self.ventValveAddress]
+        self.actuatorsAddresses = [self.ventValveAddress, self.mainValveAddress]
         self.actuatorsDataTypes = [ljm.constants.UINT16, ljm.constants.UINT16]
-        self.sensorsAddresses = [self.clock20Hz, self.flowMeterAddress, self.tankPressureAddress, self.outletPressureAddress]
+        self.sensorsAddresses = [self.clock20Hz, self.tankPressureAddress, self.outletPressureAddress, self.flowMeterAddress]
         self.sensorDataTypes = [ljm.constants.UINT32, ljm.constants.FLOAT32, ljm.constants.FLOAT32, ljm.constants.FLOAT32]
-        self.sensorMScaling = np.array([1,1/187.7,125,125])
+        self.sensorMScaling = np.array([1, 125, 125, 1/187.7])
         print self.sensorMScaling
-        self.sensorBScaling = np.array([0,0,-62.5,-62.5])
+        self.sensorBScaling = np.array([0, -62.5, -62.5, 0])
         self.ui = Ui_Dialog()
         self.ui.setupUi(self)
         self.openValveIcon = QtGui.QIcon("open.png")
         self.closedValveIcon = QtGui.QIcon("closed.png")
-        self.ui.ventValveButton.setIcon(self.closedValveIcon)
-        self.ui.mainValveButton.setIcon(self.closedValveIcon)
         self.ui.logButton.setIcon(self.closedValveIcon)
-        self.ui.eStopButton.setIcon(self.openValveIcon)
-        self.ui.mainValveButton.clicked.connect(self.actuateMainValve)
-        self.ui.ventValveButton.clicked.connect(self.actuateVentValve)
         self.ui.eStopButton.clicked.connect(self.eStop)
         self.ui.logButton.clicked.connect(self.log)
         self.dataLog = np.empty([0,4])
         self.dataLogPacketSize = 100
-        self.fig1, self.f1_axes = plt.subplots(ncols=1, nrows=3)
-        self.f1_axes[0].plot(np.random.rand(100))
-        self.f1_axes[1].plot(np.random.rand(100))
-        self.f1_axes[2].plot(np.random.rand(100))
-        self.addmpl(self.fig1)
+        self.fig, self.fig_axes = plt.subplots(ncols=1, nrows=3)
+        self.fig_axes[0].plot(np.random.rand(100))
+        self.fig_axes[1].plot(np.random.rand(100))
+        self.fig_axes[2].plot(np.random.rand(100))
+        self.addmpl(self.fig)
         self.csvFile = None
         self.csvFileName = ""
         # self.ui.chartLayout.addWidget(self.toolbar)
@@ -62,6 +55,10 @@ class MyDialog(QtGui.QDialog):
         print("Opened a LabJack with Device type: %i, Connection type: %i,\n"
             "Serial number: %i, IP address: %s, Port: %i,\nMax bytes per MB: %i" %
             (info[0], info[1], info[2], ljm.numberToIP(info[3]), info[4], info[5]))
+
+        ret = ljm.eReadAddresses(self.handle, len(self.actuatorsAddresses), self.actuatorsAddresses, self.actuatorsDataTypes)
+        self.ventValveState = bool(ret[0])
+        self.mainValveState = bool(ret[1])
 
     def addmpl(self, fig):
         self.canvas = FigureCanvas(fig)
@@ -77,7 +74,7 @@ class MyDialog(QtGui.QDialog):
             self.csvFileName = 'coldflow_'+str(datetime.now().strftime('%Y%m%d%H%M%S'))+'.csv'
             with open(self.csvFileName, mode='w') as data_file:
                 data_writer = csv.writer(data_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-                data_writer.writerow(['20Hz Clock', 'Tank Pressure', 'Outlet Pressure', 'Flow Rate'])
+                data_writer.writerow(['20Hz Clock', 'Tank Pressure', 'Outlet Pressure', 'Flow Rate Current', 'Flow Rate Raw'])
         if self.ui.logButton.isChecked():
             self.ui.logButton.setIcon(self.openValveIcon)
         else:
@@ -87,18 +84,21 @@ class MyDialog(QtGui.QDialog):
         try:
             ret = ljm.eReadAddresses(self.handle, len(self.sensorsAddresses), self.sensorsAddresses, self.sensorDataTypes)
             self.ui.clock20HzLCD.display(ret[0])
-            self.ui.flowMeterLCD.display(ret[1])
-            self.ui.tankPressureLCD.display(ret[2])
-            self.ui.outletPressureLCD.display(ret[3])
+            self.ui.flowMeterLCD.display(ret[3])
+            self.ui.tankPressureLCD.display(ret[1])
+            self.ui.outletPressureLCD.display(ret[2])
+
             scaled = np.copy(np.asarray(ret))
             scaled = scaled * self.sensorMScaling
             scaled = scaled + self.sensorBScaling
+            scaled = np.append(scaled, np.asarray(ret[3]))
 
             if self.dataLog.shape[0] == 0:
-            	self.dataLog = np.copy(scaled)
+                self.dataLog = np.copy(scaled)
             else:
-            	self.dataLog = np.vstack((self.dataLog, scaled))
+                self.dataLog = np.vstack((self.dataLog, scaled))
             if self.dataLog.shape[0]%10 == 0: # Only save/plot once per 100 readings
+                self.fig_axes[0].plot()
                 if(self.logData):
                     with open(self.csvFileName, mode='a') as data_file:
                         data_writer = csv.writer(data_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
@@ -106,53 +106,54 @@ class MyDialog(QtGui.QDialog):
                         data_writer.writerows(self.dataLog)
                 self.dataLog = np.empty([0,4])
         finally:
-            QtCore.QTimer.singleShot(1000, self.captureChannels)
+            QtCore.QTimer.singleShot(1, self.captureChannels)
 
     def writeActuators(self):
-    	try:
-    		# ljm.eWriteAddress(self.handle, self.ventValveAddress, ljm.constants.UINT16, not self.ventValveState)
-    		ljm.eWriteAddresses(self.handle, 
-    							len(self.actuatorsAddresses), 
-    							self.actuatorsAddresses, 
-    							self.actuatorsDataTypes, 
-    							[not self.mainValveState, not self.ventValveState])
-    	finally:
-    		QtCore.QTimer.singleShot(100, self.writeActuators)
+        try:
+            if self.ui.mainValveButton.isChecked():
+                self.ui.mainValveButton.setIcon(self.openValveIcon)
+                self.mainValveState = True
+            else:
+                self.ui.mainValveButton.setIcon(self.closedValveIcon)
+                self.mainValveState = False
+
+            if self.ui.ventValveButton.isChecked():
+                self.ui.ventValveButton.setIcon(self.openValveIcon)
+                self.ventValveState = True
+            else:
+                self.ui.ventValveButton.setIcon(self.closedValveIcon)
+                self.ventValveState = False
+
+            # ljm.eWriteAddress(self.handle, self.ventValveAddress, ljm.constants.UINT16, not self.ventValveState)
+            ljm.eWriteAddresses(self.handle, 
+                                len(self.actuatorsAddresses), 
+                                self.actuatorsAddresses, 
+                                self.actuatorsDataTypes, 
+                                [not self.ventValveState, not self.mainValveState])
+        finally:
+            QtCore.QTimer.singleShot(100, self.writeActuators)
 
     def actuateMainValve(self):
         print "Changing state of main valve"
         self.mainValveState = not self.mainValveState
         print self.mainValveState
         # ljm.eWriteAddress(self.handle, self.mainValveAddress, ljm.constants.UINT16, not self.mainValveState)
-        if self.ui.mainValveButton.isChecked():
-            self.ui.mainValveButton.setIcon(self.openValveIcon)
-        else:
-            self.ui.mainValveButton.setIcon(self.closedValveIcon)
 
     def actuateVentValve(self):
         print "Changing state of vent valve"
         self.ventValveState = not self.ventValveState
         print self.ventValveState
         # ljm.eWriteAddress(self.handle, self.ventValveAddress, ljm.constants.UINT16, not self.ventValveState)
-        if self.ui.ventValveButton.isChecked():
-            self.ui.ventValveButton.setIcon(self.openValveIcon)
-        else:
-            self.ui.ventValveButton.setIcon(self.closedValveIcon)
 
     def eStop(self):
         print "Estop activated"
         self.mainValveState = False
         self.ventValveState = False
+
+        self.ui.ventValveButton.setIcon(self.closedValveIcon)
+        self.ui.mainValveButton.setIcon(self.closedValveIcon)
         # aValues = [False, False]
         # ljm.eWriteAddresses(self.handle, len(aValues), self.actuatorsAddresses, self.actuatorsDataTypes, aValues)
-        if self.ui.eStopButton.isChecked():
-            self.ui.eStopButton.setIcon(self.closedValveIcon)
-        else:
-            self.ui.eStopButton.setIcon(self.openValveIcon)
-            self.ui.ventValveButton.setIcon(self.closedValveIcon)
-            self.ui.mainValveButton.setIcon(self.closedValveIcon)
-
-
 
 if __name__ == "__main__":
     app = QtGui.QApplication(sys.argv)
